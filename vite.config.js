40ac4c391c987cb94963a0b4ac15f4d81750eb84
Debasish Padhiary <padhiarydebasish@gmail.com>
2025-05-11 20:17:51 +0530

    Changes in Package.json and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/3d-portfolio/', // 👈 this is critical for GitHub Pages
   plugins: [react()],
   assetsInclude: ['**/*.glb'],
 })
